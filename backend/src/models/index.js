9b3e951e78205208a69115316746cd654903fc12
MS <mukesh.sabnis@gmail.com>
2026-07-20 16:24:40 +0100

    feat(db): add index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
 
@@ -5,17 +6,37 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: (msg) => logger.debug(msg),
   dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' ? {
-      require: true,
-      rejectUnauthorized: false
-    } : false
+    ssl: process.env.NODE_ENV === 'production'
+      ? { require: true, rejectUnauthorized: false }
+      : false,
   },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  pool: { max: 10, min: 0, acquire: 30000, idle: 10000 },
 });
 
-module.exports = { sequelize, Sequelize };
+// Auto-load all model files
+const Merchant             = require('./Merchant')(sequelize);
+const HmrcToken            = require('./HmrcToken')(sequelize);
+const BankAccount          = require('./BankAccount')(sequelize);
+const Transaction          = require('./Transaction')(sequelize);
+const VatReturn            = require('./VatReturn')(sequelize);
+const CompaniesHouseFiling = require('./CompaniesHouseFiling')(sequelize);
+const WebhookEvent         = require('./WebhookEvent')(sequelize);
+const AuditLog             = require('./AuditLog')(sequelize);
+
+const models = {
+  Merchant,
+  HmrcToken,
+  BankAccount,
+  Transaction,
+  VatReturn,
+  CompaniesHouseFiling,
+  WebhookEvent,
+  AuditLog,
+};
+
+// Set up associations
+Object.values(models).forEach((model) => {
+  if (model.associate) model.associate(models);
+});
+
+module.exports = { sequelize, Sequelize, ...models };
