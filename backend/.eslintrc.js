2b37f3a64f4288512b9a8cb98eff85ade556adb0
MS <mukesh.sabnis@gmail.com>
2026-07-20 21:57:11 +0100

    chore: add backend ESLint config

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'commonjs',
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+  },
+};
