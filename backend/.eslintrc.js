e26adf9db52e20321b510f8dde80645d77a08d28
MS <mukesh.sabnis@gmail.com>
2026-07-20 23:07:21 +0100

    fix(lint): ecmaVersion 2022 for async generators, allow empty catch

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true,
+    es2022: true,
     jest: true,
   },
   extends: ['eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 2022,
     sourceType: 'commonjs',
   },
   rules: {
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     'no-console': 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
   },
 };
